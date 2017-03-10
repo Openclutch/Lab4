fbe1085e1242a69e80251e9bb7ba27143a8b82fa
lee <leelandboelsma@gmail.com>
2017-03-09 20:13:56 -0500

    added sqlite class for the helper

---
+++ app/src/main/java/lee/lab4/SQLite.java
@@ -0,0 +1,34 @@
+package lee.lab4;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class SQLite extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "DBMessages";
+    private static final int DATABASE_VERSION = 2;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table DBMessages " +
+            "(message text not null);";
+
+    public SQLite(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(SQLite.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS messages");
+        onCreate(db);
+    }
+}
